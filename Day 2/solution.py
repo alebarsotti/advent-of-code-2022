1a587acefd557994ea3cdbc07649ecd90ec407c5
Alejandro Barsotti <alejandro.a.barsotti@gmail.com>
2022-12-02 20:57:56 -0300

    Add Day 2 solutions

---
+++ Day 2/solution.py
@@ -0,0 +1,32 @@
+from Utils.Color import print_color, Color
+
+
+values = {
+    "A": ("rock", 0),
+    "B": ("paper", 0),
+    "C": ("scissors", 0),
+    "X": ("rock", 1),
+    "Y": ("paper", 2),
+    "Z": ("scissors", 3),
+}
+
+rules = {
+    "rock": lambda other: 6 if other == "scissors" else 0,
+    "paper": lambda other: 6 if other == "rock" else 0,
+    "scissors": lambda other: 6 if other == "paper" else 0,
+}
+
+score = 0
+with open('input.txt', 'r') as file:
+    for line in file:
+        opponent, ours = line.strip().split(' ')
+        score += values[ours][1]
+        opponent = values[opponent][0]
+        ours = values[ours][0]
+        if opponent == ours:
+            score += 3
+        else:
+            score_calculator = rules[ours]
+            score += score_calculator(opponent)
+
+print(f'Total score: {print_color(score, Color.PURPLE)}')
---
+++ Day 2/solution2.py
@@ -0,0 +1,66 @@
+from Utils.Color import print_color, Color
+
+WIN = "Z"
+
+DRAW = "Y"
+
+LOSE = "X"
+
+WINNER = "winner"
+
+LOSER = "loser"
+
+ROCK = "A"
+
+PAPER = "B"
+
+SCISSORS = "C"
+
+element_values = {
+    ROCK: 1,
+    PAPER: 2,
+    SCISSORS: 3,
+}
+
+result_values = {
+    LOSE: 0,
+    DRAW: 3,
+    WIN: 6,
+}
+
+results = {
+    LOSE: lambda element: get_element(element, LOSER),
+    DRAW: lambda element: element,
+    WIN: lambda element: get_element(element),
+}
+
+
+def get_element(element, search_mode=WINNER):
+    return rules[element][search_mode]
+
+
+rules = {
+    ROCK: {
+        LOSER: SCISSORS,
+        WINNER: PAPER
+    },
+    PAPER: {
+        LOSER: ROCK,
+        WINNER: SCISSORS
+    },
+    SCISSORS: {
+        LOSER: PAPER,
+        WINNER: ROCK
+    },
+}
+
+score = 0
+with open('input.txt', 'r') as file:
+    for line in file:
+        opponent, result = line.strip().split(' ')
+        element_calculator = results[result]
+        element_to_play = element_calculator(opponent)
+
+        score += element_values[element_to_play] + result_values[result]
+
+print(f'Total score: {print_color(score, Color.PURPLE)}')
