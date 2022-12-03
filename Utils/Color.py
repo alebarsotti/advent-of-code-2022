48f5f8dfe691e5ddf9c9724281b5ce2aa11ac472
Alejandro Barsotti <alejandro.a.barsotti@gmail.com>
2022-12-02 21:11:30 -0300

    Fix cyan colors, add orange

--- Utils/Color.py
+++ Utils/Color.py
@@ -1,11 +1,12 @@
 class Color:
     PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARK_CYAN = '\033[36m'
+    LIGHT_CYAN = '\033[96m'
+    CYAN = '\033[36m'
     BLUE = '\033[94m'
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
     RED = '\033[91m'
+    ORANGE = '\033[38;2;255;128;0m'
 
 
 END = '\033[0m'
